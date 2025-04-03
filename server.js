77f007f83b6fb84c9e5e22d9c0eed6e0e4ce5471
Aayush Nandan <121042190+AayushNandan623@users.noreply.github.com>
2025-04-03 18:55:00 +0530

    learned nx

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ import client from "./client.js";
 async function settingNew(key, value) {
   await client.set(`user:${key}`, `${value}`);
 }
+async function settingNewOnlyOnce(key, value) {
+  await client.setnx(`user:${key}`, `${value}`);
+}
 
 async function getValue(key) {
   const str = await client.get(`user:${key}`);
   console.log(str);
 }
 
-settingNew(1, "Aayush");
+settingNew(1, "Abcd");
 getValue(1);
+settingNewOnlyOnce(2, "Xyz");
+getValue(2);
+settingNewOnlyOnce(2, "qwerty");
+getValue(2);
